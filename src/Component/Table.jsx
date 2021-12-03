9a4a670217014af33999fb6c864760ad11382e95
abhi9720 <abhishektiwari9720@gmail.com>
2021-12-03 22:05:39 +0530

    update -2

--- src/Component/Table.jsx
+++ src/Component/Table.jsx
@@ -7,39 +7,51 @@ import { ExportCsv, ExportPdf } from '@material-table/exporters';
 import { Link } from '@material-ui/core';
 
 const empList = [
-    { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, status: 'underVerification', expEditAlert: "1" },
-    { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 9812345678, status: 'verifiedRestricted', expEditAlert: "1" },
-    { id: 3, name: "David", email: 'david342@gmail.com', phone: 7896536289, status: 'suspended', expEditAlert: "0" },
-    { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9087654320, status: 'suspended', expEditAlert: "0" },
-    { id: 5, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, status: 'fullVerified', expEditAlert: "1" },
-    { id: 6, name: "Raj", email: 'raj@gmail.com', phone: 9812345678, status: 'suspended', expEditAlert: "1" },
-    { id: 7, name: "David", email: 'david342@gmail.com', phone: 7896536289, status: 'fullVerified', expEditAlert: "1" },
-    { id: 8, name: "Vikas", email: 'vikas75@gmail.com', phone: 9187654321, status: 'fullVerified', expEditAlert: "1" },
-    { id: 9, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, status: 'suspended', expEditAlert: "0" },
-    { id: 10, name: "Raj", email: 'raj@gmail.com', phone: 9812345678, status: 'verifiedRestricted', expEditAlert: "0" },
-    { id: 11, name: "David", email: 'david342@gmail.com', phone: 7896536289, status: 'verifiedRestricted', expEditAlert: "0" },
-    { id: 12, name: "Vikas", email: 'vikas75@gmail.com', phone: 9087654321, status: 'underVerification', expEditAlert: "0" },
+    { id: 1, name: "Neeraj", linkedin: "abhi9720", email: 'neeraj@gmail.com', phone: 9876543210, status: 'underVerification', expEditAlert: "1" },
+    { id: 2, name: "Raj", linkedin: "abhi9720", email: 'raj@gmail.com', phone: 9812345678, status: 'verifiedRestricted', expEditAlert: "1" },
+    { id: 3, name: "David", linkedin: "abhi9720", email: 'david342@gmail.com', phone: 7896536289, status: 'suspended', expEditAlert: "0" },
+    { id: 4, name: "Vikas", linkedin: "abhi9720", email: 'vikas75@gmail.com', phone: 9087654320, status: 'suspended', expEditAlert: "0" },
+    { id: 5, name: "Neeraj", linkedin: "abhi9720", email: 'neeraj@gmail.com', phone: 9876543210, status: 'fullVerified', expEditAlert: "1" },
+    { id: 6, name: "Raj", linkedin: "abhi9720", email: 'raj@gmail.com', phone: 9812345678, status: 'suspended', expEditAlert: "1" },
+    { id: 7, name: "David", linkedin: "abhi9720", email: 'david342@gmail.com', phone: 7896536289, status: 'fullVerified', expEditAlert: "1" },
+    { id: 8, name: "Vikas", linkedin: "abhi9720", email: 'vikas75@gmail.com', phone: 9187654321, status: 'fullVerified', expEditAlert: "1" },
+    { id: 9, name: "Neeraj", linkedin: "abhi9720", email: 'neeraj@gmail.com', phone: 9876543210, status: 'suspended', expEditAlert: "0" },
+    { id: 10, name: "Raj", linkedin: "abhi9720", email: 'raj@gmail.com', phone: 9812345678, status: 'verifiedRestricted', expEditAlert: "0" },
+    { id: 11, name: "David", linkedin: "abhi9720", email: 'david342@gmail.com', phone: 7896536289, status: 'verifiedRestricted', expEditAlert: "0" },
+    { id: 12, name: "Vikas", linkedin: "abhi9720", email: 'vikas75@gmail.com', phone: 9087654321, status: 'underVerification', expEditAlert: "0" },
 ]
 
 function Table() {
 
     const [data, setData] = useState(empList);
     const columns = [
-        { title: "ID", field: "id", editable: false, filtering: false },
+        {
+            title: "ID", field: "id", editable: false, filtering: false,
+            width: "3%",
+        },
         {
             title: "Name", field: "name",
+            editable: false,
             render: rowData => {
 
-                return <Link target="_blank" href={'https://localhost:9000' + rowData.name}
-
-                >
+                return <Link target="_blank" href={'https://localhost:9000' + rowData.name}>
                     {rowData.name}
                 </Link>
             },
 
         },
-        { title: "Email", field: "email" },
-        { title: "Phone Number", field: 'phone', },
+        {
+            title: "linkedin", field: "linkedin",
+            render: rowData => {
+
+                return <Link target="_blank" href={'https://www.linkedin.com/in/' + rowData.name}>
+                    {"linkedin/" + rowData.linkedin}
+                </Link>
+            },
+
+        },
+        { title: "Email", field: "email", editable: false, },
+        { title: "Phone Number", field: 'phone', editable: false, },
         {
             title: "Status", field: "status",
             lookup: {
@@ -56,8 +68,6 @@ function Table() {
                 1: 'mark verified'
             }
         },
-
-
     ]
 
     const tableIcons = {
@@ -85,6 +95,7 @@ function Table() {
         <MaterialTable
             data={data}
             columns={columns}
+
             options={{
                 // ...
                 exportMenu: [{
@@ -101,8 +112,6 @@ function Table() {
             icons={[
                 tableIcons
             ]}
-
-
             editable={{
                 onBulkUpdate: (changes) => {
                     return new Promise((resolve, reject) => {
@@ -130,6 +139,7 @@ function Table() {
                     return new Promise((resolve, reject) => {
                         setTimeout(() => {
                             const dataUpdate = [...data];
+                            console.log(dataUpdate)
                             // In dataUpdate, find target
                             const target = dataUpdate.find((el) => el.id === oldData.tableData.id);
                             const index = dataUpdate.indexOf(target);
---
+++ utli/axiosConfig.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import { BACKEND_URL } from '../constant.js';
+
+const axiosInstance = axios.create({
+    baseURL: BACKEND_URL
+});
+
+export default axiosInstance;
