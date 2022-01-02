528fdf5f0354a38a725d93545dda2e356c53da1e
abhi9720 <abhishektiwari9720@gmail.com>
2022-01-02 16:15:39 +0530

    remove unused

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+
 import { ToastContainer } from 'react-toastify';
 
 
